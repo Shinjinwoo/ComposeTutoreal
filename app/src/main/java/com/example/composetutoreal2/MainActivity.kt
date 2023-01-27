a8fe43d960d189a72dbace6a46ac3cb47b2d4adf
sinjinu <jinu712@gmail.com>
2023-01-27 10:43:16 +0900

    Google Code Lab 학습하기, Compose 기초 및 화면 쪼개기

--- app/src/main/java/com/example/composetutoreal2/MainActivity.kt
+++ app/src/main/java/com/example/composetutoreal2/MainActivity.kt
@@ -38,108 +38,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorial2Theme() {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    //MessageCard(Message("Android", "Jetpack Compose"))
-                    Conversation(SampleData.conversationSample)
-
-
-
-
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-
-data class Message(
-    val author: String,
-    val body: String
-)
-
-
 @Composable
-fun MessageCard(msg: Message) {
-// Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.error, CircleShape)
-        )
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
-
-        // We keep track if the message is expanded or not in this
-        // variable
-        var isExpanded by remember { mutableStateOf(false) }
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-        )
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.titleMedium
-            )
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 1.dp,
-                color = surfaceColor,
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    // If the message is expanded, we display all its content
-                    // otherwise we only display the first line
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-
-            var context = LocalContext.current
-            Button(
-
-                onClick = {
-                    Toast.makeText(context, "토스트 메세지 띄우기 입니다.", Toast.LENGTH_SHORT).show()
-                },
-                shape = MaterialTheme.shapes.medium,
-                colors =  ButtonDefaults.buttonColors(),
-                modifier = Modifier.wrapContentSize()
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_send_24),
-                    contentDescription = "send"
-                )
-                Spacer(modifier = Modifier.width(1.dp))
-                Text("토스트")
-            }
-        }
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTutorial2Theme {
+        Greeting("Android")
     }
 }
 
 
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
 
 @Preview
 @Composable
@@ -148,23 +72,5 @@ fun PreviewConversation() {
         Conversation(SampleData.conversationSample)
     }
 }
-
-
-//@Preview(name = "Light Mode")
-//@Preview(
-//    uiMode = Configuration.UI_MODE_NIGHT_YES,
-//    showBackground = true,
-//    name = "Dark Mode"
-//)
-//@Composable
-//fun PreviewMessageCard() {
-//    ComposeTutoreal2Theme {
-//        Surface {
-//            MessageCard(
-//                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
-//            )
-//        }
-//    }
-//}
 
 
---
+++ app/src/main/java/com/example/composetutoreal2/tutorialScreen.kt
@@ -0,0 +1,116 @@
+package com.example.composetutoreal2
+
+import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class tutorialScreen {
+
+
+
+}
+
+data class Message(
+    val author: String,
+    val body: String
+)
+
+    @Composable
+    fun MessageCard(msg: Message) {
+// Add padding around our message
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.error, CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) {
+                            Int.MAX_VALUE
+                        } else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+                var context = LocalContext.current
+                Button(
+
+                    onClick = {
+                        Toast.makeText(context, "토스트 메세지 띄우기 입니다.", Toast.LENGTH_SHORT).show()
+                    },
+                    shape = MaterialTheme.shapes.medium,
+                    colors =  ButtonDefaults.buttonColors(),
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_send_24),
+                        contentDescription = "send"
+                    )
+                    Spacer(modifier = Modifier.width(1.dp))
+                    Text("토스트")
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)
+            }
+        }
+    }
+
