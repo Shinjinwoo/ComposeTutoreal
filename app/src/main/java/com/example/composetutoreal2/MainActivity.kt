f83253c446e46cad8108eb0e167208c8d30858b9
sinjinu <jinu712@gmail.com>
2023-01-31 15:29:48 +0900

    슬롯 API를 활용한 바텀 네비게이션 부터 레이아웃 그려보기

--- app/src/main/java/com/example/composetutoreal2/MainActivity.kt
+++ app/src/main/java/com/example/composetutoreal2/MainActivity.kt
@@ -1,73 +1,33 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
 package com.example.composetutoreal2
 
 
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.Image
-
-import androidx.compose.ui.tooling.preview.Preview
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetutoreal2.ui.theme.ComposeTutorial2Theme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ComposeTutorial2Theme {
-                // A surface container using the 'background' color from the theme
-                //MyApp(modifier = Modifier.fillMaxSize())
-                //OnboardingScreen()
-
-//                SearchBar()
-//                AlignYourBodyElement(
-//                    text = R.string.inversion,
-//                    drawable = R.drawable.ab1_inversions,
-//                    modifier = Modifier.padding(8.dp)
-//                )
-
-//                FavoriteCollectionCard(
-//                    stringRes = R.string.fc2_nature_meditations,
-//                    drawable = R.drawable.fc2_nature_meditations,
-//                    modifier = Modifier.padding(8.dp)
-//                )
-
-//                AlignYourBodyRow()
-                FavoriteCollectionsGrid()
+                Scaffold(
+                    bottomBar = { SootheBottomNavigation() }
+                ) { padding ->
+                    HomeScreen(Modifier.padding(padding))
+                }
             }
         }
     }
@@ -78,168 +38,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposeTutorial2Theme {
-        //MyApp()
-        //SearchBar()
-//        AlignYourBodyElement(
-//            text = R.string.inversion,
-//            drawable = R.drawable.ab1_inversions,
-//            modifier = Modifier.padding(8.dp)
-//        )
-//        FavoriteCollectionCard(
-//            stringRes = R.string.fc2_nature_meditations,
-//            drawable = R.drawable.fc2_nature_meditations,
-//            modifier = Modifier.padding(8.dp)
-//        )
-        //AlignYourBodyRow()
         FavoriteCollectionsGrid()
     }
 }
 
-@Composable
-fun FavoriteCollectionsGrid(
-    modifier: Modifier = Modifier
-) {
-    LazyHorizontalGrid(
-        rows = GridCells.Fixed(2),
-        contentPadding = PaddingValues(horizontal = 16.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = modifier.height(120.dp)
-    ) {
-        items(favoriteCollectionsData){item ->
-            FavoriteCollectionCard(item.drawable,item.text )
-        }
-    }
-}
 
 
-@Composable
-fun FavoriteCollectionCard(
-    @DrawableRes drawable: Int,
-    @StringRes stringRes: Int
-) {
-    Surface(
-        shape = MaterialTheme.shapes.small,
-        //modifier = modifier
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.width(192.dp)
-        ) {
-            Image(
-                painter = painterResource(id = drawable),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(56.dp)
-            )
-            Text(
-                text = stringResource(stringRes),
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-        }
-    }
-}
-
-
-@Composable
-fun SearchBar(modifier: Modifier = Modifier) {
-    Surface(
-
-    ) {
-        TextField(
-            value = "",
-            onValueChange = {},
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "검색 아이콘"
-                )
-            },
-            colors = TextFieldDefaults.textFieldColors(
-                containerColor = MaterialTheme.colorScheme.surface
-            ),
-            placeholder = {
-                Text(stringResource(R.string.search))
-            },
-            modifier = modifier
-                .fillMaxWidth()
-                .heightIn(min = 56.dp)
-        )
-    }
-}
-
-
-
-@Composable
-fun AlignYourBodyRow (
-    modifier: Modifier = Modifier
-) {
-    LazyRow(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp)
-    ) {
-        items(alignYourBodyData) { item ->
-            AlignYourBodyElement(Modifier ,item.drawable, item.text)
-        }
-    }
-}
-
-@Composable
-fun AlignYourBodyElement(
-    modifier: Modifier = Modifier,
-    @DrawableRes
-    drawable: Int,
-    @StringRes
-    text: Int
-) {
-    Surface(
-
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-        ) {
-            Image(
-                painter = painterResource(drawable),
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(88.dp)
-                    .clip(CircleShape)
-            )
-            Text(
-                text = stringResource(text),
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.paddingFromBaseline(
-                    top = 24.dp, bottom = 8.dp
-                )
-            )
-        }
-    }
-}
-
-private val alignYourBodyData = listOf(
-    R.drawable.ab1_inversions to R.string.ab1_inversions,
-    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
-    R.drawable.ab3_stretching to R.string.ab3_stretching,
-    R.drawable.ab4_tabata to R.string.ab4_tabata,
-    R.drawable.ab5_hiit to R.string.ab5_hiit,
-    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
-).map { DrawableStringPair(it.first, it.second) }
-
-private val favoriteCollectionsData = listOf(
-    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
-    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
-    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
-    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
-    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
-    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
-).map { DrawableStringPair(it.first, it.second) }
-
-private data class DrawableStringPair(
-    @DrawableRes val drawable: Int,
-    @StringRes val text: Int
-)
 
